93869c8c09c00197b926b99d41929a3de0e17cbb
deyaa-pozan <deyaa.pozan@gmail.com>
2021-05-25 15:48:46 +0300

    edit about me website

--- Lab02/js/app.js
+++ Lab02/js/app.js
@@ -3,29 +3,32 @@ let userName = prompt("What is your name")
 document.getElementById("name").textContent = userName;
 
 alert("hi " + userName)
+alert("Your answer should be yes/y or no/n ")
+let Q1 = prompt("Q1 :Do I like football?").toLocaleLowerCase()
+let Q2 = prompt("Q2 :Do I love Mansaf?").toLocaleLowerCase()
+let Q3 = prompt("Q3 :Am I 22 years old?").toLocaleLowerCase()
+let Q4 = prompt("Q4 :Am I a programmer?").toLocaleLowerCase()
+let Q5 = prompt("Q5 :Am I from Syria??").toLocaleLowerCase()
 
-let Q1 = prompt("Do you have a business?").toLocaleLowerCase()
-let Q2 = prompt("Do you need a website?").toLocaleLowerCase()
-let Q3 = prompt("Do you need a Mobile Application?").toLocaleLowerCase()
-let Q4 = prompt("Do you have money is very important ? :)").toLocaleLowerCase()
-let arr = [Q1,Q2,Q3,Q4]
-var count = 0;
-arr.forEach(element => {
-    switch (element) {
-        case "yes": count++
+let arr = [Q1, Q2, Q3, Q4, Q5]
+for (let i = 0; i < arr.length; i++) {
+    
+    switch (arr[i]) {
+        case "yes": alert(`Q${i+1}Your answer is correct`)
+        break;
+        case "y": alert(`Q${i+1}Your answer is correct`)
             break;
-        case "y": count++
+        case "no":alert(`Q${i+1}Your answer is wrong`) 
+        break;
+        case "n": alert(`Q${i+1}Your answer is wrong`) 
             break;
         default:
             break;
     }
-});
 
-if(count >=3){
-    alert("contact with me 0799999999")
-}else{
-    alert("welcome my portfolio")
-}
+}    
+
+
 
 
 
